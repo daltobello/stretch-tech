63c8506d0eb008a41fe0d06a6176068958eb6128
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-10-26 09:14:17 -0400

    Add API calls with fetch for departments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getAllMuseumDepartments } from './apiCalls';
 
 function App() {
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    getAllMuseumDepartments()
+    .then(departmentsData => console.log(departmentsData))
+    .catch(error => setError(error.message))
+  }, [])
+
   return (
     <div className="App">
       <h1>Stretch Tech!</h1>
