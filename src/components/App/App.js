e66756e129f2983d6c0543142d479600439ca79f
Patrick Eitel <patrickeitel12@gmail.com>
2023-10-28 15:05:12 -0700

    Add a button to ArtCard.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,7 @@ function App() {
         // console.log("object ids: ", objectIDs);
 
         if (objectIDs.length > 0) {
-          return getDepartmentObjects(objectIDs);
+          return getDepartmentObjects(objectIDs, 80, setDepartmentObj);
         } else {
           console.log('no IDs to fetch details for');
           return [];
@@ -30,7 +30,7 @@ function App() {
         console.log('DEPARTOBJ', departmentObj)
       })
       .catch(error => {
-        setError(error.message);
+        setError(error.message)
       });
   }, []);
 
--- src/components/ArtCard/ArtCard.js
+++ src/components/ArtCard/ArtCard.js
@@ -1,16 +1,17 @@
 import './ArtCard.css'
 import { Link } from 'react-router-dom'
 
-function ArtCard({ image, title, id }) {
+function ArtCard({ image, title, id, makeFavorite}) {
   return (
-    
     <div className='art-card'>
-      <Link to={`/art/${id}`} className='selected-art-link'>
-        <img alt={`photo for ${title} art piece`} src={image}className='art-card-img'/>
-        <p>{title}</p>
-      </Link>
+      <div className='image-container'>        
+        <Link to={`/art/${id}`} className='selected-art-link'>
+          <img alt={`photo for ${title} art piece`} src={image}className='art-card-img'/>
+          <button onClick={null}>Button</button>
+         <p>{title}</p>
+        </Link>
+      </div>
     </div>
-
   )
 }
 
