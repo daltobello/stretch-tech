b637d8c15fa57fe8f2069364c4deafbb1e2c27ba
Prissilla Escobar <prissilla988@gmail.com>
2023-10-28 10:50:57 -0600

    Add link to image for selectedMovie page

--- src/components/ArtCard/ArtCard.js
+++ src/components/ArtCard/ArtCard.js
@@ -1,12 +1,16 @@
 import './ArtCard.css'
 import { Link } from 'react-router-dom'
 
-function ArtCard({ image, title }) {
+function ArtCard({ image, title, id }) {
   return (
+    
     <div className='art-card'>
-      <img src={image}className='art-card-img'/>
-      <p>{title}</p>
+      <Link to={`/art/${id}`} className='selected-art-link'>
+        <img alt={`photo for ${title} art piece`} src={image}className='art-card-img'/>
+        <p>{title}</p>
+      </Link>
     </div>
+
   )
 }
 
