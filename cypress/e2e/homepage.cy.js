bc657d1c2709215751e86ec893211fb01faf7d66
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-10-31 19:42:34 -0400

    Add homepage cypress spec

---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,53 @@
+describe("page load", () => {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11",
+      {
+        statusCode: 200,
+        fixture: "objectIDs",
+      }
+    );
+
+    cy.intercept(
+      "GET",
+      "https://collectionapi.metmuseum.org/public/collection/v1/objects/436044",
+      {
+        statusCode: 200,
+        fixture: "object-436044",
+      }
+    ).as("getObject-436044");
+
+    cy.intercept(
+      "GET",
+      "https://collectionapi.metmuseum.org/public/collection/v1/objects/436048",
+      {
+        statusCode: 200,
+        fixture: "object-436048",
+      }
+    ).as("getObject-436048");
+
+    cy.intercept(
+      "GET",
+      "https://collectionapi.metmuseum.org/public/collection/v1/objects/436049",
+      {
+        statusCode: 200,
+        fixture: "object-436049",
+      }
+    ).as("getObject-436049");
+
+    cy.visit("http://localhost:3000");
+    cy.wait("@getObject-436044");
+    cy.wait("@getObject-436048");
+    cy.wait("@getObject-436049");
+  });
+
+  it("should display all homepage elements", () => {
+    
+    // check header
+    // favorites button
+    // gallery section with 3 pictures
+    // footer
+
+  })
+});
