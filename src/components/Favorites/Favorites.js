53e449571a6a0a357fb062e148d1db651df8e3c3
Prissilla Escobar <prissilla988@gmail.com>
2023-10-30 18:27:12 -0600

    Create Favorites component that uses the favorites reducer to then map through and render an ArtCard

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,10 +1,40 @@
+import { getSingleArtDetails } from '../../apiCalls';
 import './Favorites.css'
-
+import { useSelector } from 'react-redux';
+import ArtCard from '../ArtCard/ArtCard';
+import { useEffect, useState } from 'react';
 
 function Favorites() {
+  const favoriteCards = useSelector((state) => state.favoriteCards);
+
+  const [allFaves, setAllFaves] = useState([]);
+
+  useEffect(() => {
+    const fetchFaves = async () => {
+      const fetchedFaves = await Promise.all(
+        favoriteCards.map(async (id) => {
+          const data = await getSingleArtDetails(id)
+
+          return (
+            <ArtCard 
+              id={data.objectID}
+              key={data.objectID}
+              date={data.objectDate}
+              image={data.primaryImage}
+              title={data.title}
+              artist={data.artistAlphaSort}
+            />
+          )
+        })
+      )
+      setAllFaves(fetchedFaves)
+    }
+    fetchFaves()
+  }, [favoriteCards])
+
   return (
     <div className='favorites-container'>
-      <p>Favorites</p>
+      {allFaves}
     </div>
   )
 }
