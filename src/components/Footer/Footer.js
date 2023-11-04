661128d46f9da3848fd72ac9392c2c5cb3cdf7dd
Johann Dee <johanndee1994@gmail.com>
2023-11-03 21:21:34 -0700

    Update footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,9 @@ function Footer() {
     <div className='footer'>
       <p className="footer-container">
         Disclaimer: <br></br>This website is not affiliated with The Metropolitan Museum of Art. <br></br>It is purely for educational purposes.</p>
+      <p className="repo">
+        <a href="https://github.com/daltobello/stretch-tech" className='repo-link'>GitHub</a>
+      </p>
     </div>
       
   )
