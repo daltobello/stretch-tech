7df44270230350b9d1cee3a7b2626cb07f616999
Johann Dee <johanndee1994@gmail.com>
2023-11-03 21:04:29 -0700

    Update favorites button styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,37 @@
 import './Header.css'
 import { Link } from 'react-router-dom'
 import PropTypes from "prop-types"
+import { useState } from 'react'
+import { FaRegHeart, FaHeart } from "react-icons/fa6";
 
 function Header({ resetError }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div className='header'>
       <Link to='/' className='home-link' onClick={() => {resetError()}}>
         <h1>THE MET</h1>
       </Link>
       <Link to='/favorites' className='favorites-link'>
-        <button className='fav-button'>Favorites</button>
+          <button 
+          className='fav-button'
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          >
+          {isHovered ? (
+            <span className="long-text">FAVORITES</span>
+            ) : (  
+              <span className="short-text"><FaRegHeart/></span>
+            )}
+          </button>
       </Link>  
     </div>
   )
